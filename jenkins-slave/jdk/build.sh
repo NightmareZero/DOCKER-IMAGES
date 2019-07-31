91607a7aa276aab331b8d3b8797f40578e11000b
Nightmare <xueyang.zhu@timeondata.com>
2019-07-31 18:51:26 +0800

    修改

---
+++ jenkins-slave/jdk/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source /etc/profile
+
+REGISTRY=""
+TEAM="nightzhuxy"
+NAME="jenkins-slave"
+TAG="jdk"
+
+FULLNAME="$REGISTRY$TEAM/$NAME:$TAG"
+
+{
+  { 
+    docker build -t "$FULLNAME" . 
+  } || {
+    echo -e "\e[31merror on build $FULLNAME\e[0m" && \
+    exit 1 
+  }
+} && {
+  docker push "$FULLNAME" || echo -e "\e[31merror on push\e[0m"
+}
+
