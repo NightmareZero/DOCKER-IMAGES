3588f5a7e75f5e0711649802008dc8c921017477
Nightmare <nightzhuxy@hotmail.com>
2023-10-10 10:34:01 +0800

    添加一些替换过源的linux发行版

---
+++ linux/debian11-tsinghua/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+source /etc/profile
+
+REGISTRY=""
+TEAM="nightzhuxy"
+NAME="debian"
+TAG="11-slim"
+
+FULLNAME="$REGISTRY$TEAM/$NAME:$TAG"
+
+echo 'target tag : '$FULLNAME
+sleep 3
+
+{
+  { 
+    docker build -t "$FULLNAME" . 
+  } || {
+    echo -e "\e[31merror on build $FULLNAME\e[0m" && \
+    exit 1 
+  }
+} && {
+  docker push "$FULLNAME" || echo -e "\e[31merror on push\e[0m"
+}
+
